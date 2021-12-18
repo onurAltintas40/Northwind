7aed14f3c2b0451d20ad4f170742ecb8131d84ad
Onur ALTINTAŞ <onuraltintas@hotmail.com>
2021-12-18 14:26:10 +0300

    18.12.2021 14.26

---
+++ Northwind.Entity/Mapper/MappingProfile.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using Northwind.Entity.Dto;
+using Northwind.Entity.Models;
+
+namespace Northwind.Entity.Mapper
+{
+    public class MappingProfile:Profile
+    {
+
+        public MappingProfile()
+        {
+            CreateMap<Customer, DtoCustomer>().ReverseMap();
+            //CreateMap<Customer, ViewCustomer>().ReverseMap();
+            CreateMap<Order, DtoOrder>().ReverseMap();
+        }
+    }
+}
---
+++ Northwind.Interface/IGenericService.cs
@@ -0,0 +1,28 @@
+﻿using Northwind.Entity.IBase;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace Northwind.Interface
+{
+    public interface IGenericService<T, TDto> where T : IEntityBase where TDto : IDtoBase
+    {
+        List<TDto> GetAll();
+        List<TDto> GetAll(Expression<Func<T, bool>> expression);
+        TDto Find(int id);
+        IQueryable<T> GetIQueryable();
+        TDto Add(TDto item);
+        Task<TDto> AddAsync(TDto item);
+        TDto Update(TDto item);
+        Task<TDto> UpdateAsync(TDto item);
+        bool Delete(int id);
+        Task<bool> DeleteAsync(int id);
+        bool Delete(TDto item);
+        Task<bool> DeleteAsync(TDto item);
+
+        //filtrelemeye göre silmek eklenebilir.
+        //kayıt sayısı alınabilir.
+    }
+}
